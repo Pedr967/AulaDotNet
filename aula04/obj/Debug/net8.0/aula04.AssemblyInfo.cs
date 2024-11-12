ed7ca7c4bb5c069842d0a6bbd85d04086959901d
Pedr967 <pedrohspinheiro@unipam.edu.br>
2024-11-12 13:36:14 +0000

    codigos txt

--- aula04/obj/Debug/net8.0/aula04.AssemblyInfo.cs
+++ aula04/obj/Debug/net8.0/aula04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d5501e68d4543a1efc22ff475cd9b36121e4e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d7585dbabc1882c154e00c720ca10b0a020536")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
